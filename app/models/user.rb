ee56aa65c57936dc0d918a5d8cccafb2a0095cf5
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-03 21:37:28 +0900

    Userモデルにバリデーション設定
    nickname（10文字以内）、prefecture、cityが入力必須であるバリデーションを追加。
    パスワードが半角英数字混合であるバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,16 @@ class User < ApplicationRecord
   belongs_to_active_hash :sex
   belongs_to_active_hash :age
 
-  with_options numericality: { other_than: 1, message: 'を選んで下さい' } do
+  with_options numericality: { other_than: 1 } do
     validates :sex_id, :age_id
+  end
+
+  validates :nickname, presence: true, length: { maximum: 10 }
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+
+  with_options presence: true do
+    validates :prefecture, :city
   end
 end
