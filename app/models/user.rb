ed4b62ed42739ab8a583d1756303a55b8244fcab
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-03 21:12:42 +0900

    userモデルのActiveHash設定
    ActiveHashのアソシエーションとバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :sex
+  belongs_to_active_hash :age
+
+  with_options numericality: { other_than: 1, message: 'を選んで下さい' } do
+    validates :sex_id, :age_id
+  end
 end
