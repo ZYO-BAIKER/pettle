6837d2f2890f3c7fa6ad532f0dcfc535153e53af
ZYO-BAIKER <69523136+ZYO-BAIKER@users.noreply.github.com>
2021-01-03 15:06:51 +0900

    userマイグレーションのカラム追加
    active_hashで実装予定の性別、年齢カラムの追加

--- db/migrate/20210102135627_devise_create_users.rb
+++ db/migrate/20210102135627_devise_create_users.rb
@@ -4,12 +4,13 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :prefecture,         null: false, default: ""
-      t.string :city,               null: false, default: ""
-      
+      t.string  :nickname,           null: false, default: ""
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :prefecture,         null: false, default: ""
+      t.string  :city,               null: false, default: ""
+      t.integer :sex_id,             null: false
+      t.integer :age_id,             null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
--- db/schema.rb
+++ db/schema.rb
@@ -13,11 +13,13 @@
 ActiveRecord::Schema.define(version: 2021_01_02_135627) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "nickname", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "prefecture", default: "", null: false
     t.string "city", default: "", null: false
+    t.integer "sex_id", null: false
+    t.integer "age_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
